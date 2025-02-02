25bb380b506ddea2a533ae827b014cda473cb301
GabrielSilva87 <gabri2012mi@gmail.com>
2025-02-02 16:27:35 -0300

    Atualizar o

---
+++ lucros.sql
@@ -0,0 +1,10 @@
+CREATE TABLE lucros (
+    mes VARCHAR(20),
+    ano INT,
+    lucro DECIMAL(10, 2)
+);
+
+INSERT INTO lucros (mes, ano, lucro) VALUES
+('Janeiro', 2023, 15000.00),
+('Fevereiro', 2023, 20000.00),
+('Março', 2023, 25000.00);
